48150dcf86eaf1685784ac6cae1ed49d06eccb9a
parinexus <parinexus@gmail.com>
2025-06-11 01:03:12 +0330

    test

--- data/src/main/java/parinexus/tmdb/movies/data/utils/Constant.kt
+++ data/src/main/java/parinexus/tmdb/movies/data/utils/Constant.kt
@@ -7,7 +7,7 @@ object Constant {
     const val  MOVIES_DATABASE_NAME = "movies_database"
 
 
-    const val TRENDING = "trending"
+    const val TRENDING = "trendinga"
     const val POPULAR = "popular"
     const val TOP_RATED = "top_rated"
     const val UPCOMING = "upcoming"
